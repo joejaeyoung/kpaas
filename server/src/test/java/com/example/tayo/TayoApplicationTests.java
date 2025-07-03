c5bcb89395026b4a0e63dd57c24816cec7005a08
조재영 <whwodud0303@naver.com>
2025-07-03 11:18:14 +0900

    init project

---
+++ server/src/test/java/com/example/tayo/TayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
